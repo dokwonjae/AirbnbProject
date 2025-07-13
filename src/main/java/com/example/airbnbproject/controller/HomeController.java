682ffdc4564ee7b2eac316bb0e0ba032bb0ff1e0
dobear7 <dobear7@daum.net>
2025-07-13 22:46:25 +0900

    feat: HomeController 및 index 화면 생성

---
+++ src/main/java/com/example/airbnbproject/controller/HomeController.java
@@ -0,0 +1,10 @@
+package com.example.airbnbproject.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class HomeController {
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
